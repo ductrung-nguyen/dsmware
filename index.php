6b22bf80364135eaa7266f37547b5b4f48db97a1
loveallufev <ductrungkhtn@gmail.com>
2013-05-16 21:34:28 +0200

    add index.php , controllers , views, models directories, router.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * File : index.php
+ * User : loveallufev
+ * Date:  5/16/13
+ * Group: Hieu-Trung
+*/
+
+/**
+ * WEB_ROOT_FOLDER is the name of the parent folder you created these
+ * documents in.
+ */
+//define('SERVER_ROOT' , '/var/www');
+define('SERVER_ROOT', ' /home/loveallufev/PhpstormProjects/dsmware');
+
+//yoursite.com is your webserver
+define('SITE_ROOT' , 'http://localhost');
+
+/**
+ * Fetch the router
+ */
+//require_once(SERVER_ROOT . '/controllers/router.php');
+require_once(SERVER_ROOT . '/controllers/router.php');
